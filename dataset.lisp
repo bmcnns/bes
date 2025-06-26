f266e8471175436c645fb4604a928ab2c73ee00b
Bryce MacInnis <brycemacinnis@Bryces-MacBook-Pro.local>
2025-06-26 09:40:21 -0300

    Adds a macro/DSL for loading and using offline RL datasets

---
+++ dataset.lisp
@@ -0,0 +1,25 @@
+                                        ; Macros for data loading
+(defmacro defdataset (name)
+  (let* ((dataset-name (string-trim "*" (symbol-name name)))
+         (file-name (concatenate 'string "datasets/" dataset-name)))
+    `(defparameter ,name
+       (with-open-file (in ,file-name :direction :input)
+         (read in)))))
+
+(defun batch (dataset start end)
+  (subseq dataset start end))
+
+(defun observations (transitions)
+  (mapcar #'car transitions))
+
+(defun actions (transitions)
+  (mapcar #'cadr transitions))
+
+(defun rewards (transitions)
+  (mapcar #'caddr transitions))
+
+(defun terminations (transitions)
+  (mapcar #'cadddr transitions))
+
+(defun truncations (transitions)
+  (mapcar (lambda (x) (car (cddddr x))) transitions))

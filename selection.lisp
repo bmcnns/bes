85d07d9621dc665e8eddb158452ec8c1848b56f8
Bryce MacInnis <brycemacinnis@Bryces-MacBook-Pro.local>
2025-06-30 13:44:36 -0300

    Adds a replacement method using tournament selection

--- selection.lisp
+++ selection.lisp
@@ -2,5 +2,9 @@
   (let* ((tournament-size (experiment-tournament-size experiment))
          (tournament (loop repeat tournament-size
                            collect (random-choice ranked-population))))
-    (cons tournament (argmax tournament #'cdr))))
+    (car (argmax tournament #'cdr))))
   
+(defun select (ranked-population experiment)
+  (let ((desired-population-size (experiment-population-size experiment)))
+    (loop repeat desired-population-size
+          collect (tournament-selection ranked-population experiment))))

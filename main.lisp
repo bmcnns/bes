2ec4e66d96ec0a5bef0345ea38dc0d3cf1fb6e7f
Bryce MacInnis <brycemacinnis@Bryces-MacBook-Pro.local>
2025-06-30 13:45:52 -0300

    Implements the main evolutionary loop

--- main.lisp
+++ main.lisp
@@ -1,23 +1,42 @@
+(ql:quickload :lparallel)
 (load "~/Repos/bes/utils.lisp")
 (load "~/Repos/bes/genotype.lisp")
+(load "~/Repos/bes/macros.lisp")
 (load "~/Repos/bes/instructions.lisp")
+(load "~/Repos/bes/dataset.lisp")
+(load "~/Repos/bes/hopper.lisp")
 (load "~/Repos/bes/experiments.lisp")
 (load "~/Repos/bes/phenotype.lisp")
-(load "~/Repos/bes/macros.lisp")
 (load "~/Repos/bes/mutation.lisp")
+(load "~/Repos/bes/mutation.tests.lisp")
+(load "~/Repos/bes/fitness.lisp")
 
+(defun evolutionary-loop (experiment dataset population generation)
+  (if (>= generation (experiment-generations experiment))
+      ;; Base case: Generation count exceeded, return final population
+      population
+      ;; Recursive case: Evolutionary loop
+      (let* ((batch (sample dataset experiment))
+             (obs (observations batch))
+             (acts (actions batch))
+             (num-threads (experiment-num-threads experiment))
+             (next-population
+               (let ((ranked-population
+                       (with-population population num-threads
+                         (let ((predictions (phenotype individual experiment obs)))
+                           (fitness individual #'negative-mean-squared-error acts predictions)))))
+                 (-> ranked-population
+                     (select experiment)
+                     (with-population num-threads
+                       (mutate individual experiment))))))
+        (format t "Generation ~A complete.~%" (1+ generation))
+        (force-output)
+        (evolutionary-loop experiment dataset next-population (1+ generation)))))
 
-(defun new-population (experiment)
-  (loop repeat (experiment-population-size experiment)
-        collect (new-individual experiment)))
+(defun evolve (experiment dataset)
+  (let* ((population-size (experiment-population-size experiment))
+         (initial-population (loop repeat population-size
+                                   collect (new-individual experiment))))
+    (evolutionary-loop experiment dataset initial-population 0)))
 
-(defun evolve (experiment)
-  (let ((population (new-population experiment))
-        (generations (experiment-generations experiment)))
-    (print "Running search")
-    (loop repeat generations
-          do (print "New generation"))
-    (print "Search complete")
-    population))
-
-
+      

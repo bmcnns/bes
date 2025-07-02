0580461ded17e40961b39ccb5dd5448a177409ca
Bryce MacInnis <brycemacinnis@Bryces-MacBook-Pro.local>
2025-07-02 10:34:00 -0300

    Modifies hyperparameters to be more aligned with Stephen Kelly's TPG implementation

--- experiments.lisp
+++ experiments.lisp
@@ -50,19 +50,19 @@
    :num-threads 8
    ; search
    :population-size 1000
-   :generations 10
+   :generations 1000
    ; initialization 
    :minimum-program-length 8
    :maximum-program-length 128
-   :observation-probability 0.2
-   :constant-probability 0.2
+   :observation-probability 0.5
+   :constant-probability 0.5
    ;; mutation
    :mutate-instruction-probability 1.0
    :mutate-register-probability 0.5
    :mutate-operation-probability 0.25
    :mutate-constant-probability 0.25
-   :add-instruction-probability 0.5
-   :delete-instruction-probability 0.5
-   :swap-instruction-probability 0.2
-   :constant-mutation-std 0.5
+   :add-instruction-probability 1.0
+   :delete-instruction-probability 1.0
+   :swap-instruction-probability 1.0
+   :constant-mutation-std 1.0
    :maximum-instruction-count 256)

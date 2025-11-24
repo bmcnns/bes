3d333d56c8ef503212b1b925307a6341bf817c96
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2025-11-24 12:55:22 -0400

    Adds external memory but does not implement it into TPG

---
+++ memory.lisp
@@ -0,0 +1,13 @@
+(in-package :bes)
+
+(defparameter *memory-size* 8)
+
+(defvar *memory* (make-array *memory-size* :initial-element 0.0 :element-type 'single-float))
+
+(declaim (type (simple-array single-float (*)) *memory*))
+
+(defun reset-memory ()
+  (fill *memory* 0.0f0))
+
+
+modified   mutation

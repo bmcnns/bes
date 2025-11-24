7c3cc1a32f50a245f71eefa1d860711ea6a289c1
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2025-11-24 12:58:52 -0400

    Adds an experiment config for Pendulum

---
+++ experiments/pendulum.lisp
@@ -0,0 +1,62 @@
+(ql:quickload :bes)
+(in-package :bes)
+
+(setf *random-state* (make-random-state t))
+
+(defdataset *Minimal-Pendulum-Expert-v1*
+  :path "~/.datasets/Minimal-Pendulum-Expert-v1")
+
+(defdataset *Pendulum-Expert-v1*
+  :path "~/.datasets/Pendulum-Expert-v1")
+
+(setf *dataset* (subseq *Pendulum-Expert-v1* 0 1000))
+
+(defexperiment *Pendulum-v1*
+  :batch-size 1000
+  :instruction-set (ADD SUB MUL DIV SIN COS LOG EXP)
+  :registers (R from 1 to 8) 
+  :observations (OBS from 1 to 3)
+  :output-registers (R from 1 to 1)
+  :constant-range '(-10.0 10.0)
+  :objectives (accuracy)
+  :tournament-size 4
+  :num-threads 8
+  :population-size 10000
+  :generations 10
+  :minimum-program-length 1
+  :maximum-program-length 100
+  :observation-probability 0.5
+  :constant-probability 0.5
+  :mutate-instruction-probability 1.0
+  :mutate-register-probability 0.5
+  :mutate-operation-probability 0.25
+  :mutate-constant-probability 0.25
+  :add-instruction-probability 1.0
+  :remove-instruction-probability 1.0
+  :swap-instruction-probability 1.0
+  :constant-mutation-std 1.0
+  :maximum-instruction-count 256
+  :initial-minimum-number-of-learners 2
+  :initial-maximum-number-of-learners 5
+  :actions (actions *minimal-pendulum-expert-v1*)
+  :minimum-number-of-learners 2
+  :maximum-number-of-learners 10
+  :mutate-learner-probability 0.3
+  :add-learner-probability 0.7
+  :remove-learner-probability 0.7
+  :mutate-learner-program-vs-action-probability 0.66
+  :learner-atomic-action-probability 0.5
+  :mutate-team-probability 1.0)
+
+(defparameter *experiment* *Pendulum-v1*)
+(defparameter *eval-fn* (make-execute-on-dataset-fn *dataset*))
+(defparameter *results-folder* (format nil "/Users/brycemacinnis/experiments/pendulum/~A/" (substitute #\- #\: (timestamp))))
+
+(ensure-directories-exist *results-folder*) 
+
+(evolve #'breeder *eval-fn*
+        :mode 'tpg
+        :budget 150
+        :log-file (concatenate 'string *results-folder* "scores.dat")
+        :save-file (concatenate 'string *results-folder* "agent.tpg"))
+

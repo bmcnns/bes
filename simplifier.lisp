8374f1f9eab1a2e0bf56c025f202a055648c9ca6
Bryce MacInnis <Bryce.MacInnis@dal.ca>
2025-11-24 12:54:35 -0400

    Makes a conservative post-processing step that prevents cycles

--- simplifier.lisp
+++ simplifier.lisp
@@ -89,3 +89,7 @@
 (defun postprocess (tpg team-id env-name)
   (clear-cache)
   (break-all-chains (remove-hitch-hikers (prune-tpg (rebuild-tpg tpg (list team-id))) team-id env-name)))
+
+(defun safe-postprocess (tpg team-id env-name)
+  (clear-cache)
+  (break-all-chains (prune-tpg (rebuild-tpg tpg (list team-id)))))
